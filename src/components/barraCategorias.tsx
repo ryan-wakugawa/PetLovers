a6eac400418d386be72809c0e5535b53114fd165
ryan-wakugawa <ryan.wakugawa@fatec.sp.gov.br>
2024-06-17 22:18:01 -0300

    Adiciona importação

---
+++ src/components/barraCategorias.tsx
@@ -0,0 +1,28 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../index.css'; // Custom styles
+import React from 'react';
+
+interface Props{
+    tema: string,
+    categorias: string[],
+    seletorView: Function
+}
+
+function BarraCategorias ({ tema, categorias, seletorView }: Props){
+    return (
+        <div className="d-flex btn-group btn-group-toggle my-3 mx-5" data-toggle="buttons">
+            {categorias.map((categoria, index) => (
+                <button
+                    key={index}
+                    className="btn btn-outline-secondary"
+                    onClick={(evento) => seletorView(categoria, evento)}
+                    style={{ background: tema, width: 100 }}
+                >
+                    {categoria}
+                </button>
+            ))}
+        </div>
+    );
+};
+
+export default BarraCategorias;
